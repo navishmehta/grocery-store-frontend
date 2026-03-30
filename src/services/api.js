4d196ed9383a317c8f9a8b707707ec39818cbd40
MacBook <macbook@MacBooks-MacBook-Pro.local>
2026-03-30 18:36:38 +0530

    update backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: "https://grocery-store-backend-dc6p.onrender.com/"
 });
 
 export default API;
